dc5b6d51d3eb3e18ed9eb7540b4c797a8cd966b7
Alaikum <carangiadamo@gmail.com>
2023-02-03 13:02:22 +0100

    Milestone 2

--- src/org/lessons/java/eventi/Main.java
+++ src/org/lessons/java/eventi/Main.java
@@ -1,44 +1,72 @@
 package org.lessons.java.eventi;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-	LocalDate dTest=LocalDate.of(3000, 10, 12);
-	LocalDate dTest2=LocalDate.of(4000,10,22);
+
+		Evento eve;
+		LocalDate lDate;
+		int postiTotale, giorno, mese, anno, prenotazioni,disdette;
+		String titolo,disdire;
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("Inserisci il TITOLO dell'evento: ");
+		titolo = s.nextLine();
+		System.out.println("Inserisci il numero di POSTI dell'evento: ");
+		postiTotale = Integer.parseInt(s.nextLine());
+
+		// INSERIRE LDATE
+		System.out.println("Inserisci il GIORNO dell'evento(1-31): ");
+		giorno = Integer.parseInt(s.nextLine());
+
+		System.out.println("Inserisci il MESE dell'evento(1-12): ");
+		mese = Integer.parseInt(s.nextLine());
+
+		System.out.println("Inserisci il ANNO dell'evento(es 2023): ");
+		anno = Integer.parseInt(s.nextLine());
+		lDate = LocalDate.of(anno, mese, giorno);
+
 		try {
-			Evento evento=new Evento("Ciao", dTest, 2);
-			System.out.println(evento.getLd());
-			evento.setLd(dTest2);
-			System.out.println(evento.getLd());
-			System.out.println("wewewewew");
-			System.out.println(evento.getPostiPrenotati()+"/"+evento.getPostiTotale());
-			System.out.println(evento.toString());
-			try {
-				System.out.println("controllo disdici");
-				evento.prenota();
-				System.out.println(evento.getPostiPrenotati()+"/"+evento.getPostiTotale());
-						evento.disdici();
-			} catch (Exception e) {
-				System.out.println("ATTENZIONE: "+e.getMessage());
+			eve = new Evento(titolo, lDate, postiTotale);
+			// CHIEDERE QUANTE PRENOTAZIONI FARE
+			System.out.println("Quanti posti vuoi prenotare per l'evento?");
+			prenotazioni = Integer.parseInt(s.nextLine());
+
+			for (int i = 0; i < prenotazioni; i++) {
+				eve.prenota();
 			}
-			System.out.println(evento.getPostiPrenotati()+"/"+evento.getPostiTotale());
-		} catch (PastDate e) {
-			System.out.println("ATTENZIONE: "+e.getMessage());
+			System.out.printf("Posti Prenotati: %d\nPosti ancora Disponibili: %d\n",eve.getPostiPrenotati(),eve.getPostiTotale()-eve.getPostiPrenotati());
+
+			//Chiedere all’utente se e quanti posti vuole disdire
+			System.out.println("Vuoi disdire qualche posto?\nS/N: ");
+			disdire=s.nextLine();
+			if(disdire.trim().toLowerCase().equals("s")) {
+				System.out.println("Quanti posti vuoi disdire?");
+				disdette=Integer.parseInt(s.nextLine());
+				
+				for (int i = 0; i < disdette; i++) {
+								eve.disdici();
+								
+				}
+				
+			}
 			
-		}catch(ExistingPlaces e) {
-			System.out.println("ATTENZIONE: "+e.getMessage());
+			System.out.printf("Posti Prenotati: %d\nPosti ancora Disponibili: %d\n",eve.getPostiPrenotati(),eve.getPostiTotale()-eve.getPostiPrenotati());
+			
+			
+			
+			
+			
+		} catch (Exception e) {
+			System.out.println("ATTENZIONE : " + e.getMessage());
 		}
-		
-		
-		
-		
-		System.out.println("we");
-	
-		
-//		Stiamo lavorando a un programma che deve gestire eventi (ad esempio concerti,
+
+		s.close();
+
+		// Stiamo lavorando a un programma che deve gestire eventi (ad esempio concerti,
 //				conferenze, spettacoli,...)
 //				MILESTONE 1
 //				La consegna è di creare una classe Evento che abbia le seguenti proprietà:
@@ -49,9 +77,9 @@ public class Main {
 //				Quando si istanzia un nuovo evento questi attributi devono essere tutti valorizzati nel
 //				costruttore , tranne posti prenotati che va inizializzato a 0.
 //				Inserire il controllo che la data non sia già passata e che il numero 
-		//di posti totali sia positivo.
+		// di posti totali sia positivo.
 //				In caso contrario sollevare opportune eccezioni.
-		
+
 //				Aggiungere metodi getter e setter in modo che:
 //				● titolo sia in lettura e in scrittura
 //				● data sia in lettura e scrittura
@@ -59,19 +87,20 @@ public class Main {
 //				● numero di posti prenotati sia solo in lettura
 //				Vanno inoltre implementati dei metodi public che svolgono le seguenti funzioni:
 //				1. prenota : aggiunge uno ai posti prenotati. Se l’evento è già passato o 
-		//non ha posti
+		// non ha posti
 //				disponibili deve sollevare un’eccezione.
-		
+
 //				2. disdici : riduce di uno i posti prenotati. Se l’evento è già passato o non ci sono
 //				prenotazioni deve sollevare un’eccezione.
-		
+
 //				3. l’override del metodo toString() in modo che venga restituita una stringa
 //				contenente: data formattata - titolo
 //				Aggiungete eventuali metodi (public e private) che vi aiutino a svolgere le funzioni richieste.
 //			
-		
-		//MILESTONE 2
-//				1. Creare una classe Main di test, in cui si chiede all’utente di inserire un nuovo evento
+
+		// MILESTONE 2
+//				1. Creare una classe Main di test, in cui si chiede all’utente
+		// di inserire un nuovo evento
 //				con tutti i parametri.
 //				2. Dopo che l’evento è stato istanziato, chiedere all’utente se e quante prenotazioni
 //				vuole fare e provare ad effettuarle, implementando opportuni controlli e gestendo
@@ -81,6 +110,7 @@ public class Main {
 //				5. Provare ad effettuare le disdette, implementando opportuni controlli e gestendo
 //				eventuali eccezioni
 //				6. Stampare a video il numero di posti prenotati e quelli disponibili
+
 //				MILESTONE 3
 //				Creare una classe Concerto che estende Evento, che ha anche gli attributi :
 //				● ora: LocalTime
--- src/org/lessons/java/eventi/NoReservations.java
+++ src/org/lessons/java/eventi/NoReservations.java
@@ -2,6 +2,6 @@ package org.lessons.java.eventi;
 
 public class NoReservations extends Exception {
 NoReservations(){
-	super("Non hai prenotazioni per questo eventi");
+	super("Non ci sono altre prenotazioni per questo evento");
 }
 }
